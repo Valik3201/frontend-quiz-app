e525c939dfce3be8e7ccbd684d5b6e1fc78067e3
Valik <valik3201@gmail.com>
2024-02-25 13:23:58 +0100

    refactor: update layout and refactor code

--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import data from "../data/data.json";
 import ErrorMessage from "./ErrorMessage";
+import ThemeSwitcher from "./ThemeSwitcher";
+import { getClassname } from "../utils/utils";
 
 function QuizQuestion() {
   const { quizTitle, questionIndex } = useParams();
@@ -89,7 +91,7 @@ function QuizQuestion() {
       input.disabled = false;
     });
 
-    shuffledOptions.forEach((option, index) => {
+    shuffledOptions.forEach((_option, index) => {
       const optionElement = document.querySelector(
         `li:nth-child(${index + 1})`
       );
@@ -108,85 +110,88 @@ function QuizQuestion() {
 
   return (
     <>
-      <div>
+      <div className="flex justify-between items-center mt-12">
         <div className="flex items-center gap-4 md:gap-6 text-xl font-medium">
           <img
             src={selectedQuiz.icon}
             alt={`${selectedQuiz.title} icon`}
-            className="w-10 h-10 md:w-12 md:h-12 p-1.5 md:p-2 rounded-md md:rounded-lg bg-pure-white"
+            className={`w-10 h-10 md:w-12 md:h-12 p-1.5 md:p-2 rounded-md md:rounded-lg ${getClassname(
+              selectedQuiz.title
+            )}`}
           />
           <h2>{selectedQuiz.title}</h2>
         </div>
-
-        <div className="flex justify-between w-full">
-          <div className="flex flex-col gap-7">
-            <p className="text-base italic text-grey-navy dark:text-light-bluish select-all">
-              Question {parseInt(questionIndex, 10) + 1} of{" "}
-              {selectedQuiz.questions.length}
-            </p>
-            <p className="text-2xl font-medium max-w-[35rem]">
-              {currentQuestion.question}
-            </p>
-          </div>
-        </div>
+        <ThemeSwitcher />
       </div>
 
-      <div className="text-base font-medium w-full lg:w-[564px]">
-        <ul className="flex flex-col gap-3 mt-10 md:mt-16 lg:mt-0 lg:gap-6">
-          {shuffledOptions.map((option, index) => (
-            <li key={index} className="group">
-              <input
-                type="radio"
-                id={index}
-                name="option"
-                className="hidden peer"
-                onChange={handleOptionChange}
-              />
-              <label
-                htmlFor={index}
-                className={`inline-flex items-center justify-between w-full cursor-pointer bg-pure-white dark:bg-navy rounded-xl md:rounded-3xl p-3 lg:p-4 border-[3px] border-pure-white dark:border-navy transition duration-300 ease-in-out hover:border-purple dark:hover:border-purple shadow-light dark:shadow-dark ${
-                  selectedAnswer === index && isAnswerSubmitted
-                    ? "group-[.correct]:border-green group-[.incorrect]:border-red"
-                    : "peer-checked:border-purple"
-                }`}
-              >
-                <div className="inline-flex items-center gap-4 md:gap-8">
-                  <div
-                    className={`transition duration-300 ease-in-out flex items-center justify-center  w-10 max-h-10 md:min-w-12 md:max-h-12 p-1.5 md:p-2 rounded-md md:rounded-lg ${
-                      selectedAnswer === index
-                        ? isAnswerSubmitted
-                          ? "group-[.correct]:bg-green group-[.correct]:text-pure-white group-[.incorrect]:bg-red group-[.incorrect]:text-pure-white"
-                          : "bg-purple text-pure-white"
-                        : "bg-light-grey text-grey-navy group-hover:bg-[#f6e7ff] group-hover:text-purple"
-                    }`}
-                  >
-                    {String.fromCharCode(97 + index).toUpperCase()}
+      <div className="flex justify-between w-full flex-wrap mt-12">
+        <div className="flex flex-col gap-7">
+          <p className="text-base italic text-grey-navy dark:text-light-bluish select-all">
+            Question {parseInt(questionIndex, 10) + 1} of{" "}
+            {selectedQuiz.questions.length}
+          </p>
+          <p className="text-2xl font-medium max-w-[35rem]">
+            {currentQuestion.question}
+          </p>
+        </div>
+
+        <div className="text-base font-medium w-full lg:w-[564px]">
+          <ul className="flex flex-col gap-3 mt-10 md:mt-16 lg:mt-0 lg:gap-6">
+            {shuffledOptions.map((option, index) => (
+              <li key={index} className="group">
+                <input
+                  type="radio"
+                  id={index}
+                  name="option"
+                  className="hidden peer"
+                  onChange={handleOptionChange}
+                />
+                <label
+                  htmlFor={index}
+                  className={`inline-flex items-center justify-between w-full cursor-pointer bg-pure-white dark:bg-navy rounded-xl md:rounded-3xl p-3 lg:p-4 border-[3px] border-pure-white dark:border-navy transition duration-300 ease-in-out hover:border-purple dark:hover:border-purple shadow-light dark:shadow-dark ${
+                    selectedAnswer === index && isAnswerSubmitted
+                      ? "group-[.correct]:border-green group-[.incorrect]:border-red"
+                      : "peer-checked:border-purple"
+                  }`}
+                >
+                  <div className="inline-flex items-center gap-4 md:gap-8">
+                    <div
+                      className={`transition duration-300 ease-in-out flex items-center justify-center  w-10 max-h-10 md:min-w-12 md:max-h-12 p-1.5 md:p-2 rounded-md md:rounded-lg ${
+                        selectedAnswer === index
+                          ? isAnswerSubmitted
+                            ? "group-[.correct]:bg-green group-[.correct]:text-pure-white group-[.incorrect]:bg-red group-[.incorrect]:text-pure-white"
+                            : "bg-purple text-pure-white"
+                          : "bg-light-grey text-grey-navy group-hover:bg-[#f6e7ff] group-hover:text-purple"
+                      }`}
+                    >
+                      {String.fromCharCode(97 + index).toUpperCase()}
+                    </div>
+
+                    <div>{option}</div>
                   </div>
 
-                  <div>{option}</div>
-                </div>
-
-                <img
-                  src="/assets/icon-correct.svg"
-                  alt="Correct Icon"
-                  className="hidden group-[.correct]:block"
-                />
-                <img
-                  src="/assets/icon-incorrect.svg"
-                  alt="Incorrect Icon"
-                  className="hidden group-[.incorrect]:block"
-                />
-              </label>
-            </li>
-          ))}
-        </ul>
-        <button
-          onClick={isAnswerSubmitted ? handleNextQuestion : handleSubmit}
-          className="text-center p-4 lg:p-6 mt-4 md:mt-6 w-full cursor-pointer bg-purple text-pure-white rounded-xl md:rounded-3xl transition duration-300 ease-in-out transform shadow-light dark:shadow-dark hover:opacity-75"
-        >
-          {isAnswerSubmitted ? "Next question" : "Submit answer"}
-        </button>
-        {errorVisible && <ErrorMessage message={errorMessage} />}
+                  <img
+                    src="/assets/icon-correct.svg"
+                    alt="Correct Icon"
+                    className="hidden group-[.correct]:block"
+                  />
+                  <img
+                    src="/assets/icon-incorrect.svg"
+                    alt="Incorrect Icon"
+                    className="hidden group-[.incorrect]:block"
+                  />
+                </label>
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={isAnswerSubmitted ? handleNextQuestion : handleSubmit}
+            className="text-center p-4 lg:p-6 mt-4 md:mt-6 w-full cursor-pointer bg-purple text-pure-white rounded-xl md:rounded-3xl transition duration-300 ease-in-out transform shadow-light dark:shadow-dark hover:opacity-75"
+          >
+            {isAnswerSubmitted ? "Next question" : "Submit answer"}
+          </button>
+          {errorVisible && <ErrorMessage message={errorMessage} />}
+        </div>
       </div>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import data from "../data/data.json";
+import ThemeSwitcher from "../components/ThemeSwitcher";
+import { getClassname } from "../utils/utils";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -11,61 +13,52 @@ const Home = () => {
     navigate(`/${quizTitle}/question/0`);
   };
 
-  function getClassname(index) {
-    switch (index % 4) {
-      case 0:
-        return "bg-[#fff1e9]";
-      case 1:
-        return "bg-[#e0fdef]";
-      case 2:
-        return "bg-[#ebf0ff]";
-      case 3:
-        return "bg-[#f6e7ff]";
-      default:
-        return "bg-[#f6e7ff]";
-    }
-  }
-
   return (
     <>
-      <div className="flex flex-col gap-12">
-        <div className="flex flex-col gap-2 select-all">
-          <span className="text-3xl md:text-4xl font-extralight">
-            Welcome to the
-          </span>
-          <span className="text-3xl md:text-4xl font-medium">
-            Frontend Quiz!
-          </span>
-        </div>
-        <p className="text-base italic text-grey-navy dark:text-light-bluish select-all">
-          Pick a subject to get started.
-        </p>
+      <div className="flex justify-end items-center mt-12">
+        <ThemeSwitcher />
       </div>
 
-      <ul className="flex flex-col gap-3 mt-10 md:mt-16 lg:mt-0 lg:gap-6 w-full lg:w-[564px]">
-        {data.quizzes.map(({ title, icon }, index) => (
-          <li
-            key={title}
-            className="cursor-pointer bg-pure-white dark:bg-navy border-[3px] border-pure-white dark:border-navy rounded-xl md:rounded-3xl transition duration-300 ease-in-out transform hover:border-purple dark:hover:border-purple shadow-light dark:shadow-dark"
-          >
-            <button
-              className="flex items-center p-3 lg:p-4 gap-4 md:gap-8 w-full"
-              onClick={() => startQuiz(title)}
+      <div className="flex justify-between w-full flex-wrap mt-12">
+        <div className="flex flex-col gap-12">
+          <div className="flex flex-col gap-2 select-all">
+            <span className="text-3xl md:text-4xl font-extralight">
+              Welcome to the
+            </span>
+            <span className="text-3xl md:text-4xl font-medium">
+              Frontend Quiz!
+            </span>
+          </div>
+          <p className="text-base italic text-grey-navy dark:text-light-bluish select-all">
+            Pick a subject to get started.
+          </p>
+        </div>
+
+        <ul className="flex flex-col gap-3 mt-10 md:mt-16 lg:mt-0 lg:gap-6 w-full lg:w-[564px]">
+          {data.quizzes.map(({ title, icon }) => (
+            <li
+              key={title}
+              className="cursor-pointer bg-pure-white dark:bg-navy border-[3px] border-pure-white dark:border-navy rounded-xl md:rounded-3xl transition duration-300 ease-in-out transform hover:border-purple dark:hover:border-purple shadow-light dark:shadow-dark"
             >
-              <img
-                src={icon}
-                alt={`${title} icon`}
-                className={`w-10 h-10 md:w-12 md:h-12 p-1.5 md:p-2 rounded-md md:rounded-lg ${getClassname(
-                  index
-                )}`}
-              />
-              <span className="text-[1.12rem] md:text-xl font-medium">
-                {title}
-              </span>
-            </button>
-          </li>
-        ))}
-      </ul>
+              <button
+                className="flex items-center p-3 lg:p-4 gap-4 md:gap-8 w-full"
+                onClick={() => startQuiz(title)}
+              >
+                <img
+                  src={icon}
+                  alt={`${title} icon`}
+                  className={`w-10 h-10 md:w-12 md:h-12 p-1.5 md:p-2 rounded-md md:rounded-lg ${getClassname(
+                    title
+                  )}`}
+                />
+                <span className="text-[1.12rem] md:text-xl font-medium">
+                  {title}
+                </span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
