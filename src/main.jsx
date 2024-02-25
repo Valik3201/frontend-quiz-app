59f16d1514db20bffa5b85c209e56eb5947b9d64
Valik <valik3201@gmail.com>
2024-02-25 20:36:22 +0100

    feat: add animations, theme provider, and update styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { ThemeProvider } from "./components/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="light" storageKey="theme">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
